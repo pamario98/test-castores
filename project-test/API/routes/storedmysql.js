5981b661f5997b1d82da9eff7613ef53db89f9cd
Mario Eduardo García González <mario@MacBook-Pro-de-Mario.local>
2022-07-04 12:44:51 -0500

    test-castores

--- project-test/API/routes/storedmysql.js
+++ project-test/API/routes/storedmysql.js
@@ -5,7 +5,7 @@ var mysql = require("mysql");
 var conn = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Naruto255?",
+  password: "",
   database: "examen_castores_noticias",
 });
 
